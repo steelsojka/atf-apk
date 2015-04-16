2b89587659567c4a9e5830394b6e1c5358c4d6fe
Steven Sojka <steelsojka@gmail.com>
2015-04-16 15:58:15 -0500

    refactor(pages): make pages better

--- gulp/tasks/pages.js
+++ gulp/tasks/pages.js
@@ -4,11 +4,28 @@ import gulp from 'gulp';
 import jade from 'gulp-jade';
 import replace from 'gulp-replace';
 import pkg from '../../package.json';
+import through from 'through2';
+import fs from 'fs';
+import path from 'path';
 
 export default gulp.task('pages', () => {
-  gulp.src('./src/*.jade')
+  gulp.src('./src/pages/*.jade')
+    .pipe(page('./src/wrapper.jade'))
     .pipe(jade({ pretty: true }))
     .pipe(replace('<!--scripts-->', `<script src='bundle.js?v=${pkg.version}'></script>`))
     .pipe(replace('<!--styles-->', `<link href='bundle.css?v=${pkg.version}' rel='stylesheet'>`))
     .pipe(gulp.dest('./build'));
 });
+
+function page(wrapper) {
+  const wrapperContent = fs.readFileSync(wrapper, { encoding: 'utf8' });
+
+  function write(file, enc, cb) {
+    file.contents = new Buffer(wrapperContent.replace('//content-include', `include ${path.relative('./src/pages', file.path)}`));
+    this.push(file);
+
+    cb();
+  }
+
+  return through.obj(write);
+}
