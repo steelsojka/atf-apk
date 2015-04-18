b35659a42d11bb4fafdda0dd56fb13acd590d2cf
Steven Sojka <steelsojka@gmail.com>
2015-04-18 08:11:17 -0500

    chore(app): work it

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -6,7 +6,7 @@ import runSequence from 'run-sequence';
 export default gulp.task('default', done => {
   runSequence(
     'clean:build',
-    ['scripts', 'pages', 'styles'],
+    ['scripts', 'pages', 'styles', 'assets'],
     ['connect', 'watch'],
     done
   );
